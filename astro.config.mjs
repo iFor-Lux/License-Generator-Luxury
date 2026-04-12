12d70c563eaa14f7239f8ca411549be4bc6c7342
Luxury . dev <jhojantomairo2@gmail.com>
2026-04-12 10:42:46 -0500

    FIX BACKGROUND

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iFor-Lux.github.io',
   base: '/License-Generator-Luxury',
+  integrations: [react()],
 });
